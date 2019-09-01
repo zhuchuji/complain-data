3876f4cf460e0dd15f11b1c3a201244c97a36cc4
zhuchuji <1012536778@qq.com>
2019-09-01 21:22:28 +0800

    build production

---
+++ docs/precache-manifest.d06461661651ffa48858435c076ce73a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9697a3ba3bd6a285bfca",
+    "url": "./static/js/main.9697a3ba.chunk.js"
+  },
+  {
+    "revision": "36348d22fae20dfdd7f7",
+    "url": "./static/js/1.36348d22.chunk.js"
+  },
+  {
+    "revision": "9697a3ba3bd6a285bfca",
+    "url": "./static/css/main.3a6dd969.chunk.css"
+  },
+  {
+    "revision": "36348d22fae20dfdd7f7",
+    "url": "./static/css/1.2e0a9f5c.chunk.css"
+  },
+  {
+    "revision": "1b40a158dd6fecf168a79b97307993ac",
+    "url": "./index.html"
+  }
+];
